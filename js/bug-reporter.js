191947099fc079559f646f7f55d8b50c082f2a3c
Jonathan Petitcolas <jonathan@marmelab.com>
2014-01-31 09:40:18 +0100

    Create basic bug report form

--- js/bug-reporter.js
+++ js/bug-reporter.js
@@ -2,5 +2,14 @@
 
     "use strict";
 
-    document.getElementsByTagName("body")[0].innerHTML = "<h1>Yippie!</h1>";
+    document.getElementById("bug-form").addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        var title = document.getElementById("bug-title").value;
+        var description = document.getElementById("bug-description").value;
+
+        console.log(title);
+        console.log(description);
+    });
+
 })();
